47bfe5e74fb524562412210c984c6d681e019278
BATIDILLO8 <BATIDILLO_8@HOTMAIL.COM>
2026-06-15 18:00:14 -0500

    Modulo GANADORES-referidos

---
+++ app/admin/notificaciones/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+export default function NotificacionesPage() {
+
+  return (
+
+    <div className="p-10">
+
+      <h1>
+        Notificaciones
+      </h1>
+
+    </div>
+
+  )
+
+}
